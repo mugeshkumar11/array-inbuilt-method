16c0a6fc68dea496fca34556a885f276dd2d7904
mugeshkumar <kmugesh11199@gmail.com>
2022-11-30 19:04:21 +0530

    add new file

--- js/app.js
+++ js/app.js
@@ -1,65 +1,70 @@
 //filter method of array
 
-// var input = [14,16,17,23,34,36,28]
+var input = [14,16,17,23,34,36,28]
 
-// var result = input.filter(item => item>18)
+var result = input.filter(item => item>18)
 
-// console.log(result);
+console.log(result);
 
 
 //for each method 
 
-// var input = [5,6,7,8,9,3];
-// var sum =0;
+var input = [5,6,7,8,9,3];
+var sum =0;
 
-// var result = input.forEach((item) => sum = sum+item)
+var result = input.forEach((item) => {sum = sum+item; console.log(sum);})
 
-// console.log(result);
+// console.log(r);
 
 
-//reduce method
+// reduce method
 
-// var input = ["javascript","css","html"];
+var input = ["javascript","css","html"];
 
-// var result = input.reduce((item,index)=>item+index);
+var result = input.reduce((item,index)=>item+index);
 
-// console.log(result);
+console.log(result);
 
 
 //indexof method
 
-// var input = ["javascript","css","html"];
+var input = ["javascript","css","html"];
 
-// var result = input.indexOf("javascript",4);
+var result = input.indexOf("script",4);
 
-// console.log(result);
+console.log(result);
 
 
 //push method
 
 var input = ["javascript","css","html"];
 
-input.push("mugesh");
-
  console.log(input);
+
+console.log(input.push("mugesh"));
+ console.log(input);
+
+
 
 
  //pop method
 
 var input = ["javascript","css","html"];
+console.log(input);
 
-input.pop();
+ console.log(input.pop());
 
- console.log(input);
+
+
 
 
  //shift method
 
 var input = ["javascript","css","html"];
 
-input.shift();
 
- console.log(input);
+ console.log(input)
+ console.log(input.shift());
 
 
  //unshift method
@@ -85,6 +90,6 @@ var result = input1.concat(input2)
 
 var input = [14,16,17,23,34,36,28]
 
-var result = input.every(item => item>18)
+var result = input.every(item => item>10)
 
 console.log(result);
